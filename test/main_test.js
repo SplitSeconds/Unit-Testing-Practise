4a6de7e9203c8e934117de7243358b993a8e81b8
Nele Luepkes <nluepkes@gmail.com>
2019-01-28 08:31:55 +0100

    added more tests

---
+++ test/main_test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+//Set up test suite (block of unit tests)
+describe('Mocha', function() {
+  // Test spec (unit test)
+  it('should run our tests using npm', function() {
+    expect(true).to.be.ok;
+  });
+})
